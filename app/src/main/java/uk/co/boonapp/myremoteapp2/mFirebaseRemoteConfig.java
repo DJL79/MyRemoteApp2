735af16b71f5b5174867fb9ec5da0528a128574c
Dylan Lawless (CA Account) <ca_K1517669@kclad.ds.kcl.ac.uk>
2017-09-06 10:22:50 +0100

    Initial commit

---
+++ app/src/main/java/uk/co/boonapp/myremoteapp2/mFirebaseRemoteConfig.java
@@ -0,0 +1,16 @@
+package uk.co.boonapp.myremoteapp2;
+
+/**
+ * Created by ca_K1517669 on 06/09/2017.
+ */
+
+class mFirebaseRemoteConfig {
+    private static final mFirebaseRemoteConfig ourInstance = new mFirebaseRemoteConfig();
+
+    static mFirebaseRemoteConfig getInstance() {
+        return ourInstance;
+    }
+
+    private mFirebaseRemoteConfig() {
+    }
+}
